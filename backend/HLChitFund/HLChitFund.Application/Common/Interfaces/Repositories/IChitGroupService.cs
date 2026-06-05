a018a537e6bf9cb5a4fa19c253537eee7fc22907
alasaneeth <alasaneeth22@gmail.com>
2026-06-05 09:26:59 +0300

    feat(application): add IChitGroupService interface

---
+++ backend/HLChitFund/HLChitFund.Application/Common/Interfaces/Repositories/IChitGroupService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLChitFund.Application.Common.Interfaces.Repositories
+{
+    internal class IChitGroupService
+    {
+    }
+}
