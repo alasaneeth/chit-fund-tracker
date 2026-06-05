3b4c9e5d28d4e6e8d87a523a7339bba5b7985960
alasaneeth <alasaneeth22@gmail.com>
2026-06-05 09:25:10 +0300

    feat(application): add ChitGroupRequestDto and ChitGroupResponseDto

---
+++ backend/HLChitFund/HLChitFund.Application/DTOs/ChitGroup/ChitGroupResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLChitFund.Application.DTOs.ChitGroup
+{
+    internal class ChitGroupResponseDto
+    {
+    }
+}
