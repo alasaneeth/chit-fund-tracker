4609906c3bc8ded036179926f7fa38dd23472e52
alasaneeth <alasaneeth22@gmail.com>
2026-06-06 12:26:18 +0300

    feat(application): add IEnrollmentService interface

---
+++ backend/HLChitFund/HLChitFund.Application/Common/Interfaces/IEnrollmentService.cs
@@ -0,0 +1,24 @@
+﻿using HLChitFund.Application.DTOs.Enrollment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HLChitFund.Application.Common.Interfaces
+{
+    public interface IEnrollmentService
+    {
+        Task<IEnumerable<EnrollmentResponseDto>>
+            GetAllEnrollmentsAsync();
+        Task<IEnumerable<EnrollmentResponseDto>>
+            GetEnrollmentsByChitGroupAsync(int chitGroupId);
+        Task<IEnumerable<EnrollmentResponseDto>>
+            GetEnrollmentsByCustomerAsync(int customerId);
+        Task<EnrollmentResponseDto?> GetEnrollmentByIdAsync(int id);
+        Task<EnrollmentResponseDto> CreateEnrollmentAsync(
+            EnrollmentRequestDto request);
+        Task<bool> DeleteEnrollmentAsync(int id);
+    }
+
+}
