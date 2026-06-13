a8c54edb771300cd6a60630226bc9cf8bd41ad76
alasaneeth <alasaneeth22@gmail.com>
2026-06-13 11:28:03 +0530

    feat(api): add WinnerController with winner selection endpoints

---
+++ backend/HLChitFund/HLChitFund.API/Controllers/WinnerController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HLChitFund.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WinnerController : ControllerBase
+    {
+    }
+}
