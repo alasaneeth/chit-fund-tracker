a8e9a101e726114c4f359cf76db4b2c79d13c12a
alasaneeth <alasaneeth22@gmail.com>
2026-06-18 15:26:42 +0530

    fix(ui): fix dashboard stats change detection

--- frontend/src/app/features/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpClient } from '@angular/common/http';
@@ -19,31 +19,42 @@ export class DashboardComponent implements OnInit {
     activeEnrollments: 0
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.loadStats();
   }
 
   loadStats(): void {
-    this.http.get<any>(`${environment.apiUrl}/ChitGroup`).subscribe({
-      next: (data) => this.stats.totalGroups = data.length,
-      error: () => {}
+    this.http.get<any[]>(`${environment.apiUrl}/ChitGroup`).subscribe({
+      next: (data) => {
+        this.stats.totalGroups = data.length;
+        this.cdr.detectChanges();
+      }
     });
 
-    this.http.get<any>(`${environment.apiUrl}/Customer`).subscribe({
-      next: (data) => this.stats.totalCustomers = data.length,
-      error: () => {}
+    this.http.get<any[]>(`${environment.apiUrl}/Customer`).subscribe({
+      next: (data) => {
+        this.stats.totalCustomers = data.length;
+        this.cdr.detectChanges();
+      }
     });
 
-    this.http.get<any>(`${environment.apiUrl}/Enrollment`).subscribe({
-      next: (data) => this.stats.activeEnrollments = data.length,
-      error: () => {}
+    this.http.get<any[]>(`${environment.apiUrl}/Enrollment`).subscribe({
+      next: (data) => {
+        this.stats.activeEnrollments = data.length;
+        this.cdr.detectChanges();
+      }
     });
 
-    this.http.get<any>(`${environment.apiUrl}/Payment`).subscribe({
-      next: (data) => this.stats.totalCollections = data.length,
-      error: () => {}
+    this.http.get<any[]>(`${environment.apiUrl}/Payment`).subscribe({
+      next: (data) => {
+        this.stats.totalCollections = data.length;
+        this.cdr.detectChanges();
+      }
     });
   }
 }
