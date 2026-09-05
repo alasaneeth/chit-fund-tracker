2ba058fcc28b44d57750a52a410b582021066158
alasaneeth <alasaneeth22@gmail.com>
2026-09-05 12:30:21 +0530

    readme update

--- backend/HLChitFund/HLChitFund.Application/Common/Interfaces/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿using HLChitFund.Application.DTOs.Auth;
-
-namespace HLChitFund.Application.Common.Interfaces;
-
-public interface IAuthService
-{
-    Task<LoginResponseDto> LoginAsync(LoginRequestDto request);
-    Task<bool> RegisterAsync(RegisterRequestDto request);
-}
